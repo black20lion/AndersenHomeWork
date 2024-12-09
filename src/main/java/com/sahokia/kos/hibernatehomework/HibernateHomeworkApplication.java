9526cfcf1e481600a691ca3b364f6605e5aa9fcf
black20lion <Kos.Sahokia@yandex.ru>
2024-12-09 17:17:55 +0400

    removed commented strings

--- src/main/java/com/sahokia/kos/hibernatehomework/HibernateHomeworkApplication.java
+++ src/main/java/com/sahokia/kos/hibernatehomework/HibernateHomeworkApplication.java
@@ -14,16 +14,16 @@ public class HibernateHomeworkApplication {
     public static void main(String[] args) {
         TicketService ticketService = new TicketService(new TicketDao(), new CustomerDao());
         System.out.println(ticketService.getCustomerById(12));
-//        System.out.println(ticketService.getTicketById(5));
-//        ticketService.createTicket(new Ticket(new Customer("Misha"), TicketType.WEEK));
-//        ticketService.createCustomer(new Customer("Maksim"));
-//        ticketService.deleteTicketById(4);
-//        ticketService.updateTicketType(3, TicketType.DAY);
-//        ticketService.deleteCustomerById(7);
-//        List<Ticket> tickets = new ArrayList<>();
-//        tickets.add(new Ticket(new Customer("Alex"), TicketType.MONTH));
-//        tickets.add(new Ticket(new Customer("Maria"), TicketType.DAY));
-//        tickets.add(new Ticket(new Customer("Kirill"), TicketType.WEEK));
-//        ticketService.updateCustomerAndTickets(12, "Boris", tickets);
+        System.out.println(ticketService.getTicketById(5));
+        ticketService.createTicket(new Ticket(new Customer("Misha"), TicketType.WEEK));
+        ticketService.createCustomer(new Customer("Maksim"));
+        ticketService.deleteTicketById(4);
+        ticketService.updateTicketType(3, TicketType.DAY);
+        ticketService.deleteCustomerById(7);
+        List<Ticket> tickets = new ArrayList<>();
+        tickets.add(new Ticket(new Customer("Alex"), TicketType.MONTH));
+        tickets.add(new Ticket(new Customer("Maria"), TicketType.DAY));
+        tickets.add(new Ticket(new Customer("Kirill"), TicketType.WEEK));
+        ticketService.updateCustomerAndTickets(12, "Boris", tickets);
     }
 }
